f9b70e21749fd778d0160f206848cbbbab1034dd
kaytee124 <ktproductions124@gmail.com>
2025-12-15 16:53:46 +0000

    alx_sql

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,11 +1,15 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password= "Vicky@2017",
-)
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Vicky@2017"
+    )
 
-cursor = mydb.cursor()
-cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-print("Database 'alx_book_store' created successfully.")
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE alx_book_store")
+    print("Database 'alx_book_store' created successfully.")
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
